9ee4d760e6da406835925833b7c16e7a0700a3c1
IrinaBaburina <irinababurina@gmail.com>
2016-11-17 16:50:51 -0400

    Созданы классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,14 @@ public class MyFirstProgram {
      hello("Irina");
      hello("friends");
 
-     double l = 4;
-     System.out.println("Площать квадрата со стороной " + l + " = " + area(l));
+     Square s = new Square(4);
+     System.out.println("Площать квадрата со стороной " + s.l + " = " + s.area());
 
-     double a = 3;
-     double b = 7;
-     System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+     Rectangle r = new Rectangle(3, 7);
+     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
    }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
   }
 }
