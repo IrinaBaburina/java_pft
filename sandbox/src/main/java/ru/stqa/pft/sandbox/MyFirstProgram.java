d2a93f051546591ef406ecafb94de7632ac728a5
IrinaBaburina <irinababurina@gmail.com>
2016-11-18 21:31:52 -0400

    Тренировка

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,43 +13,36 @@ public class MyFirstProgram {
      Rectangle r = new Rectangle(3, 7);
      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-     double z = 14;
-     double w = 4;
-     double y = 12;
-     System.out.println("Периметр треугольника со сторонами " + z + " , " + w + " и " + y + " = " + area(z, w, y));
+     Triangle t = new Triangle();
+     t.z = 14;
+     t.w = 4;
+     t.y = 12;
+     System.out.println("Периметр треугольника со сторонами " + t.z + " , " + t.w + " и " + t.y + " = " + area(t));
 
-     int a = 6;
-     int b = 18;
-     int c = 3;
-     int d = 11;
-     int n = 1;
-     System.out.println("Число " + a + " в кубе будет " + " = " + number(a));
-     System.out.println("Число " + b + " в кубе будет " + " = " + number(b));
-     System.out.println("Число " + c + " в кубе будет " + " = " + number(c));
-     System.out.println("Число " + d + " в кубе будет " + " = " + number(d));
-     System.out.println("Число " + n + " в кубе будет " + " = " + number(n));
+     Cube c = new Cube();
+     c.a = 6;
+     System.out.println("Число " + c.a + " в кубе будет " + " = " + number(c));
 
-     double g = 25;
-     double k = 4096;
-     double i = 16;
-     System.out.println("Если извлечь корень из числа " + g + " , то результат будет " + " = " + number(g));
-     System.out.println("Если извлечь корень из числа " + k + " , то результат будет " + " = " + number(k));
-     System.out.println("Если извлечь корень из числа " + i + " , то результат будет " + " = " + number(i));
+
+     Kor k = new Kor();
+     k.g = 25;
+     System.out.println("Если извлечь корень из числа " + k.g + " , то результат будет " + " = " + number(k));
+
    }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double z, double w, double y){
-    return z + w + y;
+  public static double area(Triangle t){
+    return t.z + t.w + t.y;
   }
 
-  public static int number(int a){
-    return a*a*a;
+  public static int number(Cube c){
+    return c.a*c.a*c.a;
   }
 
-  public static double number(double g){
-    return Math.sqrt(g);
+  public static double number(Kor k){
+    return Math.sqrt(k.g);
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Triangle.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by iryamka on 11/18/2016.
+ */
+public class Triangle {
+
+  public double z;
+  public double w;
+  public double y;
+
+}
