5a7817eac2b9f198329129a328130cd3f18db36c
IrinaBaburina <irinababurina@gmail.com>
2016-11-22 21:51:30 -0400

    Homework #2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,20 +4,15 @@ public class MyFirstProgram {
 
    public static void main(String[] args) {
 
-     Point p1 = new Point();
-     p1.x = 3;
-     p1.y = 5;
+     Point p1 = new Point(3, 5);
 
-     Point p2 = new Point();
-     p2.x = 7;
-     p2.y = 8;
-     
+     Point p2 = new Point(7, 8);
+
      double d = distance(p1, p2);
-     System.out.println("Расстояние между точками равно " + d);
+     System.out.println("Расстояние между точками Point1 c координатами " + " (" + p1.x + " , " + p1.y + ") " + " и Point2 с координатами " + " (" + p2.x + " , " + p2.y + ") " + " равно " + d + ".");
    }
 
    public static double distance(Point p1, Point p2){
      return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
    }
-
 }
