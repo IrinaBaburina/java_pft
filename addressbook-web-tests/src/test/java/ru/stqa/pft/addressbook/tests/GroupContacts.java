4774d48c376d199ee0a5d9f5f660f64fa8b4eb92
IrinaBaburina <irinababurina@gmail.com>
2016-12-09 15:42:39 -0400

    Перекинула в testbase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupContacts.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupContacts.java
@@ -21,30 +21,6 @@ public class GroupContacts extends TestBase {
         app.getGroupHelper().submitGroupCreation();
     }
 
-    public void fillFormContact(GroupFormContacts groupFormContacts) {
-        app.getGroupHelper().wd.findElement(By.name("firstname")).click();
-        app.getGroupHelper().wd.findElement(By.name("firstname")).clear();
-        app.getGroupHelper().wd.findElement(By.name("firstname")).sendKeys(groupFormContacts.getFirstname());
-        app.getGroupHelper().wd.findElement(By.name("lastname")).click();
-        app.getGroupHelper().wd.findElement(By.name("lastname")).clear();
-        app.getGroupHelper().wd.findElement(By.name("lastname")).sendKeys(groupFormContacts.getLastname());
-        app.getGroupHelper().wd.findElement(By.name("nickname")).click();
-        app.getGroupHelper().wd.findElement(By.name("nickname")).clear();
-        app.getGroupHelper().wd.findElement(By.name("nickname")).sendKeys(groupFormContacts.getNickname());
-        app.getGroupHelper().wd.findElement(By.name("company")).click();
-        app.getGroupHelper().wd.findElement(By.name("company")).clear();
-        app.getGroupHelper().wd.findElement(By.name("company")).sendKeys(groupFormContacts.getCompany());
-        app.getGroupHelper().wd.findElement(By.name("address")).click();
-        app.getGroupHelper().wd.findElement(By.name("address")).clear();
-        app.getGroupHelper().wd.findElement(By.name("address")).sendKeys(groupFormContacts.getAddress());
-        app.getGroupHelper().wd.findElement(By.name("mobile")).click();
-        app.getGroupHelper().wd.findElement(By.name("mobile")).clear();
-        app.getGroupHelper().wd.findElement(By.name("mobile")).sendKeys(groupFormContacts.getMobile());
-        app.getGroupHelper().wd.findElement(By.name("email")).click();
-        app.getGroupHelper().wd.findElement(By.name("email")).clear();
-        app.getGroupHelper().wd.findElement(By.name("email")).sendKeys(groupFormContacts.getEmail());
-    }
-
     public void addNewContact() {
         app.getGroupHelper().wd.findElement(By.linkText("add new")).click();
     }
