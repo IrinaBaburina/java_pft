ac3726c62f15db4d007174b79d0ca03deaad3cb1
IrinaBaburina <irinababurina@gmail.com>
2016-12-10 23:19:29 -0400

    Revert "Перекинула в testbase"
    This reverts commit 4774d48c376d199ee0a5d9f5f660f64fa8b4eb92.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,10 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-import ru.stqa.pft.addressbook.model.GroupFormContacts;
 
 /**
  * Created by iryamka on 12/7/2016.
@@ -23,27 +21,4 @@ public class TestBase {
     app.stop();
   }
 
-  public void fillFormContact(GroupFormContacts groupFormContacts) {
-      app.getGroupHelper().wd.findElement(By.name("firstname")).click();
-      app.getGroupHelper().wd.findElement(By.name("firstname")).clear();
-      app.getGroupHelper().wd.findElement(By.name("firstname")).sendKeys(groupFormContacts.getFirstname());
-      app.getGroupHelper().wd.findElement(By.name("lastname")).click();
-      app.getGroupHelper().wd.findElement(By.name("lastname")).clear();
-      app.getGroupHelper().wd.findElement(By.name("lastname")).sendKeys(groupFormContacts.getLastname());
-      app.getGroupHelper().wd.findElement(By.name("nickname")).click();
-      app.getGroupHelper().wd.findElement(By.name("nickname")).clear();
-      app.getGroupHelper().wd.findElement(By.name("nickname")).sendKeys(groupFormContacts.getNickname());
-      app.getGroupHelper().wd.findElement(By.name("company")).click();
-      app.getGroupHelper().wd.findElement(By.name("company")).clear();
-      app.getGroupHelper().wd.findElement(By.name("company")).sendKeys(groupFormContacts.getCompany());
-      app.getGroupHelper().wd.findElement(By.name("address")).click();
-      app.getGroupHelper().wd.findElement(By.name("address")).clear();
-      app.getGroupHelper().wd.findElement(By.name("address")).sendKeys(groupFormContacts.getAddress());
-      app.getGroupHelper().wd.findElement(By.name("mobile")).click();
-      app.getGroupHelper().wd.findElement(By.name("mobile")).clear();
-      app.getGroupHelper().wd.findElement(By.name("mobile")).sendKeys(groupFormContacts.getMobile());
-      app.getGroupHelper().wd.findElement(By.name("email")).click();
-      app.getGroupHelper().wd.findElement(By.name("email")).clear();
-      app.getGroupHelper().wd.findElement(By.name("email")).sendKeys(groupFormContacts.getEmail());
-  }
 }
