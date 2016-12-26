f42be48af3e6c4a4450bbe51aaa8986a62a5d90d
Jesikka <irinababurina@gmail.com>
2016-12-26 13:36:41 -0400

    W4 L6 Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupFormContacts.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupFormContacts.java
@@ -52,4 +52,31 @@ public class GroupFormContacts {
   public String getGroup() {
     return group;
   }
+
+  @Override
+  public String toString() {
+    return "GroupFormContacts{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupFormContacts that = (GroupFormContacts) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -25,5 +25,8 @@ public class ContactDeletionTest extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     List<GroupFormContacts> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
-  }
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
+    }
 }
