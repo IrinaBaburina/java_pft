fda3173edf16fd0f2e76de2747f876a57fd00014
Jesikka <irinababurina@gmail.com>
2016-12-26 20:24:11 -0400

    W4 L10 Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupFormContacts.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupFormContacts.java
@@ -25,7 +25,7 @@ public class GroupFormContacts {
   }
 
   public GroupFormContacts(String firstname, String lastname, String nickname, String company, String address, String mobile, String email, String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.nickname = nickname;
@@ -80,7 +80,6 @@ public class GroupFormContacts {
 
     GroupFormContacts contacts = (GroupFormContacts) o;
 
-    if (id != contacts.id) return false;
     if (firstname != null ? !firstname.equals(contacts.firstname) : contacts.firstname != null) return false;
     return lastname != null ? lastname.equals(contacts.lastname) : contacts.lastname == null;
 
@@ -88,8 +87,7 @@ public class GroupFormContacts {
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    int result = firstname != null ? firstname.hashCode() : 0;
     result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
     return result;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupFormContacts;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTest extends TestBase {
@@ -19,8 +19,10 @@ public class ContactCreationTest extends TestBase {
     List<GroupFormContacts> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupFormContacts> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
